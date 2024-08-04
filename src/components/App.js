3517435cbc93b1f96909eb744eef223139b86d85
eustachekamala <eustachejared25@gmail.com>
2024-08-04 17:35:26 +0300

    In this lab I used the filter method to filte the items by category, and the map to copy the array of Items without change It, there methods I used It with the useState and some event

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import { useState } from "react";
+
 
 function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Toggle function to switch between dark and light mode
+  const toggleDarkMode = () => {
+    setIsDarkMode(prevMode => !prevMode);
+  };
+
+  // Determine class based on the current mode
+  const appClass = isDarkMode ? "App dark" : "App light";
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={appClass === 'App dark' ? toggleDarkMode : toggleDarkMode}>{appClass === 'App dark' ? 'Dark Mode 🌙' : 'Light Mode ☀️'}</button>
+        {/* add a small tag with the text 'Change to Light Mode' or 'Change to Dark Mode' depending on the current mode */}
+         {/* <small style={{color: appClass === 'App dark' ? 'white' : 'black', background: appClass === 'App dark' ? 'red' : 'yellow', padding: '0.5rem'}}>{appClass === 'App dark' ? 'Change to Light Mode' : 'Change to Dark Mode'}</small> */}
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  // Keep track of whether the item is in the cart or not
+  const [inCart, setInCart] = useState(false);
+
+  // Handle click on the Add to Cart button
+  function handleCartClick() {
+    setInCart(!inCart);
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {/*Add a className of 'in-cart' to the <li> when the Add to Cart button is clicked*/}
+      <button className={inCart ? "remove-from-cart" : "add-to-cart"} onClick={handleCartClick}>
+        {inCart ? "Remove cart 🛒" : "Add to cart"}
+      </button>
     </li>
   );
 }
